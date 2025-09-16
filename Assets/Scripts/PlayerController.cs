93b79b29351128a3b7db32306cd35cec820be52f
Alan Villalobos <145631906+SanicSpeed1238@users.noreply.github.com>
2025-09-15 23:01:09 -0500

    did more work on player controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,63 +4,105 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     [Header("Movement Stats")]
+    [Range(1, 10)]
+    [SerializeField] float steerSensitivity;
+    [Range(1, 10)]
+    [SerializeField] float driftStrength;
     [Range(1,100)]
     [SerializeField] float accelerationRate;
     [Range(100,1000)]
-    [SerializeField] float maxBaseSpeed;
-    [Range(1,10)]
-    [SerializeField] float steerSensitivity;
+    [SerializeField] float maxSpeed;
+    [Range(10,100)]
+    [SerializeField] float boostRate;
+    [Range(1, 10)]
+    [SerializeField] float jumpHeight;
 
     Rigidbody playerRB;
-    float currentSpeed;
+    float baseSpeed;
+    float baseMaxSpeed;
 
-    float inputAccel;
-    float inputBoost;
-    float inputDrift;
-    float inputJump;
+    float inputAccel; 
+    float inputDrift;   
     float inputSteer;
+    bool inputBoost;
+    bool inputJump;
 
     void Start()
     {
         playerRB = GetComponentInChildren<Rigidbody>();
+        baseMaxSpeed = maxSpeed;
     }
 
     void FixedUpdate()
     {
         Steer();
-        Accelerate();     
+        Drift();
+        Accelerate();
+        Boost();
+        Jump();
     }
 
     void Accelerate()
     {
-        currentSpeed += inputAccel * accelerationRate * Time.fixedDeltaTime;
-        if (currentSpeed > maxBaseSpeed) currentSpeed = maxBaseSpeed;
+        baseSpeed += inputAccel * accelerationRate * Time.fixedDeltaTime;
+        if (baseSpeed > maxSpeed) baseSpeed = maxSpeed;
 
-        playerRB.linearVelocity = transform.forward * currentSpeed;
+        Vector3 playerVelocity = transform.forward * baseSpeed;
+        playerVelocity.y = playerRB.linearVelocity.y;
+        playerRB.linearVelocity = playerVelocity;
+    }
+
+    void Boost()
+    {
+        if (inputBoost)
+        {
+            maxSpeed += boostRate * Time.fixedDeltaTime;
+        }
+        else
+        {
+            if (maxSpeed > baseMaxSpeed) maxSpeed -= ((boostRate * 2f) * Time.fixedDeltaTime);
+            else maxSpeed = baseMaxSpeed;
+        }
+    }
+
+    void Jump()
+    {
+        if (inputJump)
+        {
+            if (Physics.Raycast(transform.position, Vector3.down, 1f))
+            {
+                playerRB.AddForce(jumpHeight * transform.up, ForceMode.Impulse);
+            }
+        }
     }
 
     void Steer()
     {
-        playerRB.MoveRotation(Quaternion.Euler(0f, inputSteer * steerSensitivity, 0f) * playerRB.rotation);
+        if(inputDrift == 0f) playerRB.MoveRotation(Quaternion.Euler(0f, inputSteer * steerSensitivity, 0f) * playerRB.rotation);
     }
 
-    public void OnAccelerate(InputAction.CallbackContext buttonInput)
+    void Drift()
     {
-        inputAccel = buttonInput.ReadValue<float>();
+        if (inputDrift != 0f) Debug.Log("Drifting");
+    }
+
+    public void OnAccelerate(InputAction.CallbackContext floatInput)
+    {
+        inputAccel = floatInput.ReadValue<float>();
         
     }
+    public void OnDrift(InputAction.CallbackContext floatInput)
+    {
+        inputDrift = floatInput.ReadValue<float>();
+
+    }    
     public void OnBoost(InputAction.CallbackContext buttonInput)
     {
-        inputBoost = buttonInput.ReadValue<float>();
-    }
-    public void OnDrift(InputAction.CallbackContext buttonInput)
-    {
-        inputDrift = buttonInput.ReadValue<float>();
-
+        inputBoost = buttonInput.ReadValueAsButton();
     }
     public void OnJump(InputAction.CallbackContext buttonInput)
     {
-        inputJump = buttonInput.ReadValue<float>();
+        inputJump = buttonInput.ReadValueAsButton();
     }
     public void OnSteer(InputAction.CallbackContext stickInput)
     {
